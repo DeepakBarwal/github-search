881e4b00fd410f8344c5b35b2cdb87e951124e08
DeepakBarwal <deepakbarwal98@gmail.com>
2022-03-06 20:42:53 +0530

    working github-search app

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Search</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React app to search Github profiles and see profile details.
+      </p>
+      <p className="text-lg text-purple-400">
+        Version <span className="font-bold">1.0.0</span>
+      </p>
+    </div>
+  );
+};
+
+export default About;
